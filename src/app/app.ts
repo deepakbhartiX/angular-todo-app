3c22995a6e30d249afab70281f4e6cae1f234414
Deepak Bharti <deepak17122002@gmail.com>
2026-05-29 11:11:22 +0530

    angular todo app

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,29 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todoapp');
+  newTodo = signal('')
+  todos  = signal<{text:string;done:boolean;}[]>([])
+
+  addTodo(){
+    const text = this.newTodo().trim()
+    if(text===''){
+      return
+    }
+    this.todos.update(list=>[...list,{text,done:false}])
+
+    this.newTodo.set('')
+
+  }
+  toggleTodo(index:number){
+    this.todos.update(list=>list.map((item,i)=> i === index?{...item,done:!item.done}:item))
+  }
+
+  deleteTodo(index:number){
+    this.todos.update(list=>list.filter((_,i)=> i !== index))
+  }
 }
